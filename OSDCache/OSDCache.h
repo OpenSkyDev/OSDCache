d368ad78d8fd0bc0abd3454bde19a8331a796c94
Skylar Schipper <ss@schipp.co>
2013-12-15 00:07:20 -0800

    Initial Commit

---
+++ OSDCache/OSDCache.h
@@ -0,0 +1,13 @@
+//
+//  OSDCache.h
+//  OSDCache
+//
+//  Created by Skylar Schipper on 12/15/13.
+//  Copyright (c) 2013 OpenSky, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OSDCache : NSObject
+
+@end
---
+++ OSDCache/OSDCache.m
@@ -0,0 +1,13 @@
+//
+//  OSDCache.m
+//  OSDCache
+//
+//  Created by Skylar Schipper on 12/15/13.
+//  Copyright (c) 2013 OpenSky, LLC. All rights reserved.
+//
+
+#import "OSDCache.h"
+
+@implementation OSDCache
+
+@end
